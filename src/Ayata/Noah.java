64dfec460aa6e6ee1dfa06d77b5f592744a22bf4
selenucar <slnucartx@gmail.com>
2021-08-04 00:16:37 -0500

    Noah rides

--- src/Ayata/Noah.java
+++ src/Ayata/Noah.java
@@ -7,6 +7,7 @@ public class Noah {
         System.out.println("Noah is 14 years old.");
         System.out.println("Noah wants to work");
         System.out.println("Noah will go to State High");
+        System.out.println("Noah will ride a school bus");
 
     }
 }
