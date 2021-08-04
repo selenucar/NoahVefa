cfc533a15ae77359eb73db7a11187bedf73eb2d2
selenucar <slnucartx@gmail.com>
2021-08-04 00:06:19 -0500

    Noah school status

--- src/Ayata/Noah.java
+++ src/Ayata/Noah.java
@@ -6,6 +6,7 @@ public class Noah {
 
         System.out.println("Noah is 14 years old.");
         System.out.println("Noah wants to work");
+        System.out.println("Noah will go to State High");
 
     }
 }
