eb6d3e026f20d39b1ad9b29296219c437d5eaa3e
selenucar <slnucartx@gmail.com>
2021-08-03 23:45:56 -0500

    Noah will work soon

--- src/Ayata/Noah.java
+++ src/Ayata/Noah.java
@@ -5,6 +5,7 @@ public class Noah {
     public static void main(String[] args) {
 
         System.out.println("Noah is 14 years old.");
+        System.out.println("Noah wants to work");
 
     }
 }
